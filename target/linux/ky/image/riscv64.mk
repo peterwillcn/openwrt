5a7fed9108fbaedcb5757d8058bfca25a4206787
baiywt <baiywt_gj@163.com>
2025-02-14 09:57:56 +0800

    Support Orange Pi RV2/R2S

---
+++ target/linux/ky/image/riscv64.mk
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2020 Sarah Maedel
+
+# FIT will be loaded at 0x02080000. Leave 16M for that, align it to 2M and load the kernel after it.
+KERNEL_LOADADDR := 0x03200000
+
+define Device/x1_orangepi-rv2
+  DEVICE_VENDOR := KY
+  DEVICE_MODEL := Orange Pi RV 2
+  SOC := x1
+  DEVICE_PACKAGES := kmod-usb-net-rtl8152
+endef
+TARGET_DEVICES += x1_orangepi-rv2
+
+define Device/x1_orangepi-r2s
+  DEVICE_VENDOR := KY
+  DEVICE_MODEL := Orange Pi R2S
+  SOC := x1
+  DEVICE_PACKAGES := kmod-usb-net-rtl8152 kmod-r8169
+endef
+TARGET_DEVICES += x1_orangepi-r2s
---
+++ target/linux/ky/riscv64/target.mk
@@ -0,0 +1,8 @@
+ARCH:=riscv64
+SUBTARGET:=riscv64
+BOARDNAME:=x1 boards (64 bit)
+
+define Target/Description
+	Build firmware image for Ky X1 devices.
+	This firmware features a 64 bit kernel.
+endef
